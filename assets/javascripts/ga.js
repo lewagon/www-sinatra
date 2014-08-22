933b378d377e6aed51c493d8da27d08d83e9c768
Boris Paillard <boris.paillard@gmail.com>
2014-08-22 11:16:31 +0200

    tracking newsletter

--- assets/javascripts/ga.js
+++ assets/javascripts/ga.js
@@ -1,14 +1,14 @@
 $(function(){
-  $("#main-nl").on("click", function(){
+  $("#Newsletter").on("click", function(){
     ga('send', 'event', 'newsletter', 'click', 'main-newsletter');
   });
-  $("#paris-nl").on("click", function(){
+  $("#ParisNewsletter").on("click", function(){
     ga('send', 'event', 'newsletter', 'click', 'paris-newsletter');
   });
-  $("#brussels-nl").on("click", function(){
+  $("#BrusselsNewsletter").on("click", function(){
     ga('send', 'event', 'newsletter', 'click', 'brussels-newsletter');
   });
-  $(".blog-aside #main-nl").on("click", function(){
+  $("#BlogNewsletter").on("click", function(){
     ga('send', 'event', 'newsletter', 'click', 'blog-newsletter');
   });
 });
