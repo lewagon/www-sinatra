2a28bb285ed853753348f908a4feb061056de437
cedricmenteau <me.cedric@gmail.com>
2014-11-25 19:09:13 +0100

    First draft

--- lib/data.rb
+++ lib/data.rb
@@ -16,5 +16,6 @@ PARTNERS = load(:partners)
 SUBVENTIONS = load(:subventions)
 CAMPS = load(:camps)
 AUTHORS = load(:authors)
+ARTICLES = load(:articles)
 
 CAMPS.delete(:test) if ENV['RACK_ENV'] == 'production'
